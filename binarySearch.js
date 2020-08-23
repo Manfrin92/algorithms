352d7d6ae33cef9fcf0c80021210a8b25afcd4f5
Manfrin92 <manfredd_10@hotmail.com>
2020-08-23 07:28:01 -0300

    feat: final code

--- binarySearch.js
+++ binarySearch.js
@@ -21,15 +21,35 @@ function isEven(array){
 function getMiddleNumberFromArray(array) {
     if (!isEven(array)) {
         const middleOddTerm = array.slice(Number.parseInt(array.length/2), Number.parseInt(array.length/2)+1);
-        return middleOddTerm;
+        return middleOddTerm[0];
     } 
-    // Will always return an array with the two numbers
-    const middleEvenTerm = [...array.slice(Number.parseInt(array.length/2-1), Number.parseInt(array.length/2)+1)];
-    return middleEvenTerm;    
+    // Will always return the first index
+    const middleEvenTerm = array.slice(Number.parseInt(array.length/2-1), Number.parseInt(array.length/2-1)+1);
+    return middleEvenTerm[0];    
 }
 
-const arrayGe = arrayGeneratorOfIntegersSorted(10);
-console.log(arrayGe);
+function getHalfOfArray(array, side ) {
+    return array.slice(0, 2)
+}
+
+function binarySearch(array, sortedNumber) {
+    let steps = 1;
+    let analyzedArray = [...array];
+    let numberInTheMiddle = getMiddleNumberFromArray(analyzedArray);
+    console.log('Original Array: ', array);
+    console.log('SortedNumber: ', sortedNumber);
+    
+    while (sortedNumber !== numberInTheMiddle) {
+        steps++;        
+        if (sortedNumber > numberInTheMiddle) {
+            analyzedArray = array.slice(array.indexOf(numberInTheMiddle)+1,); 
+        } else {
+            analyzedArray = array.slice(0, array.indexOf(numberInTheMiddle));
+        }
+        numberInTheMiddle = getMiddleNumberFromArray(analyzedArray);
+    }
+    console.log(`Found with ${steps} steps.`);
+}
+const arrayGe = arrayGeneratorOfIntegersSorted(2);
 const sortedNumberWithinTheArray = sortOneOfTheArrayNumbers(arrayGe)
-console.log('Número sorteado: ', sortedNumberWithinTheArray);
-console.log('Retornado pelo selector: ', getMiddleNumberFromArray(arrayGe))
+binarySearch(arrayGe, sortedNumberWithinTheArray);
